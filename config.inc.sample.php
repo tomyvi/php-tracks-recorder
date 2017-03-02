e4485b23d6a143444a817d46aeae22c20f6711f6
tomyvi <tomyvi@gmail.com>
2017-03-02 21:45:00 +0100

    config sample file setup

---
+++ config.inc.sample.php
@@ -0,0 +1,15 @@
+<?php
+	
+	//RENAME TO config.inc.php
+
+
+	$_config['sql_host'] = '';
+	$_config['sql_user'] = '';
+	$_config['sql_pass'] = '';
+	$_config['sql_db'] = '';
+	
+	$_config['sql_prefix'] = '';
+	
+	$_config['default_accuracy'] = 1000;
+
+?>
