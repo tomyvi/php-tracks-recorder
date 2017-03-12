c117816264ba0c64ae08734e9b325b11465e3e8a
tomyvi <tomyvi@gmail.com>
2017-03-12 10:01:59 +0100

    preping config for geo_decode

--- config.inc.sample.php
+++ config.inc.sample.php
@@ -2,6 +2,8 @@
 	
 	//RENAME TO config.inc.php
 
+	setlocale(LC_TIME, "fr_FR");
+
 
 	$_config['sql_host'] = '';
 	$_config['sql_user'] = '';
@@ -11,5 +13,8 @@
 	$_config['sql_prefix'] = '';
 	
 	$_config['default_accuracy'] = 1000;
+	
+	$_config['enable_geo_reverse'] = false;
+	$_config['geo_reverse_lookup_url'] = "http://193.63.75.109/reverse?format=json&zoom=18&accept-language=fr&addressdetails=0&email=sjobs@apple.com&";
 
 ?>
