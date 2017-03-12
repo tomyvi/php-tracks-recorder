82ca0a322d37915bf8a25aa30de701a55241a734
jeppetrost <jeppetrost@protonmail.com>
2017-03-12 12:55:46 +0100

    config-includes and file-name in leaflet
    Includes config.inc.php in record.php to have the changes actually go in
    the databse. Change to leaflet to reflect picture-file-name. Changes to
    buttons to not be links, but rather just buttons you push (means site
    does not reload).

--- record.php
+++ record.php
@@ -4,15 +4,16 @@
     # and insert into database table.
 
     header("Content-type: application/json");
+    require_once('config.inc.php');
 
     $payload = file_get_contents("php://input");
     $data =  @json_decode($payload, true);
-	
+
     if ($data['_type'] == 'location') {
 
         # CREATE TABLE locations (dt TIMESTAMP, tid CHAR(2), lat DECIMAL(9,6), lon DECIMAL(9,6));
-        $mysqli = new mysqli("127.0.0.1", "root", "", "138515_vigneron");
-		
+        $mysqli = new mysqli($_config['sql_host'], $_config['sql_user'], $_config['sql_pass'], $_config['sql_db']);
+
 		//http://owntracks.org/booklet/tech/json/
         if (array_key_exists('acc', $data)) $accuracy = $data['acc'];
         if (array_key_exists('alt', $data)) $altitude = $data['alt'];
@@ -30,7 +31,7 @@
 		if (array_key_exists('vel', $data)) $velocity = $data['vel'];
 		if (array_key_exists('p', $data)) $pressure = $data['p'];
 		if (array_key_exists('conn', $data)) $connection = $data['conn'];
-		
+
         $sql = "INSERT INTO log_locations (accuracy, altitude, battery_level, heading, description, event, latitude, longitude, radius, trig, tracker_id, epoch, vertical_accuracy, velocity, pressure, connection) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
         $stmt = $mysqli->prepare($sql);
         # bind parameters (s = string, i = integer, d = double,  b = blob)
