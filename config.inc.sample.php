cc992fdc30d10ef0cde7a5e161786b643c2e22f9
Thomas Vigneron <Thomas.Vigneron@fr.eurw.ey.net>
2017-09-01 16:03:18 +0200

    initial upload of new leafletjs plugin

--- config.inc.sample.php
+++ config.inc.sample.php
@@ -4,7 +4,6 @@
 
 	setlocale(LC_TIME, "fr_FR");
 
-
 	$_config['sql_host'] = '';
 	$_config['sql_user'] = '';
 	$_config['sql_pass'] = '';
@@ -12,9 +11,9 @@
 	
 	$_config['sql_prefix'] = '';
 	
-	$_config['default_accuracy'] = 1000;
+	$_config['default_accuracy'] = 1000; //meters
+	$_config['default_trackerID'] = 'all';
 	
-	$_config['enable_geo_reverse'] = false;
 	$_config['geo_reverse_lookup_url'] = "http://193.63.75.109/reverse?format=json&zoom=18&accept-language=fr&addressdetails=0&email=sjobs@apple.com&";
 	
 	$_config['geo_reverse_boundingbox_url'] = "http://nominatim.openstreetmap.org/reverse?format=json&osm_type=W&osm_id=";
