17b6e719a2644fe10fa0f767278accdc4f563a65
tomyvi <tomyvi@protonmail.com>
2018-03-15 16:16:24 +0100

    Update config.inc.sample.php

--- config.inc.sample.php
+++ config.inc.sample.php
@@ -2,6 +2,7 @@
 	
 	//RENAME TO config.inc.php
 
+	setlocale(LC_TIME, "fr_FR");
 
 	$_config['sql_host'] = '';
 	$_config['sql_user'] = '';
@@ -10,6 +11,11 @@
 	
 	$_config['sql_prefix'] = '';
 	
-	$_config['default_accuracy'] = 1000;
+	$_config['default_accuracy'] = 1000; //meters
+	$_config['default_trackerID'] = 'all';
+	
+	$_config['geo_reverse_lookup_url'] = "http://193.63.75.109/reverse?format=json&zoom=18&accept-language=fr&addressdetails=0&email=sjobs@apple.com&";
+	
+	$_config['geo_reverse_boundingbox_url'] = "http://nominatim.openstreetmap.org/reverse?format=json&osm_type=W&osm_id=";
 
 ?>
